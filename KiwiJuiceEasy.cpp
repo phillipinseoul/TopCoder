92b2efd922f82efd4f158c693b52e59ab08eb594
phillip9807@gmail.com <phillip9807@gmail.com>
2021-01-05 22:14:39 +0900

    init commit

---
+++ KiwiJuiceEasy.cpp
@@ -0,0 +1,27 @@
+// N bottles, capacities[i] (L), bottles[i] (L)
+// M operations: i-th -> pour from fromId[i] to toId[i]
+// Stop when: i) fromId[i] = empty ii) toId[i] = full
+// Return: int[] -> i-th: amount of juice in i-th bottle'
+
+/*
+    for (int i = 0; i < M; i++){
+        int toL = toId[i];
+        int fromL = fromId[i];
+
+        if (bottles[toL] + fromL >= 10){
+            
+        }
+        else {
+
+        }
+    }
+
+*/
+
+
+
+
+
+
+
+
